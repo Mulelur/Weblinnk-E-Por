72eef7f54b3c4a5209dd5aa415d1f5aaa30c2609
Mulelur <63222158+Mulelur@users.noreply.github.com>
2022-07-24 09:33:55 +0200

    config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,142 +1,19 @@
 export const ProjectDetails = {
   projectName: "epo-e",
   projectVersion: "1.0.0.2",
-  projectDescription: "",
-  projectAuthor: "",
-  projectAuthorEmail: "",
+  projectId: 1,
+  projectDescription: "Portfolio website for EPO-E",
+  projectAuthor: "Rotonda Mulelu",
+  projectAuthorEmail: "rotondwamulelu@gmail.com",
   projectAuthorUrl: "",
-  projectLicense: "",
+  projectLicense: "MIT",
   projectLicenseUrl: "",
   projectCopyright: "",
-  projectHomepage: "",
-  projectRepository: "",
-  projectRepositoryType: "",
-  projectRepositoryUrl: "",
-  projectRepositoryBranch: "",
 };
 
 export const user = {
   name: "Rotonda Mulelu",
   email: "rotondwamulelu@gmail.com",
   phoneNumber: "+27712051353",
-  address: "",
-  password: "",
-  confirmPassword: "",
-  role: "",
   status: "",
-  degree: "",
-  createdAt: "",
-  updatedAt: "",
-};
-
-export const homePage = {
-  logoName: "Rotonda",
-  intro: "I'm a future engineer",
-  profile: {
-    url: "https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-    desc: "Home profile picture",
-  },
-  quote:
-    "Productivity is never an accident. It’s always the result of a commitment to excellence, intelligent planning, and focused effort.",
-  text: "I am taking new projects. Waiting to hearing about new projects, so if you'd like to chat please get in touch.",
-  links: [
-    {
-      name: "linkedin",
-      link: "",
-    },
-  ],
-};
-
-export const aboutPage = {
-  intro: "Code. Eat. Sleep.",
-  names: "Rotonda Mulelu",
-  profile: {
-    url: "https://images.unsplash.com/photo-1523824921871-d6f1a15151f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-    desc: '"I am a future engineer"',
-  },
-  summary:
-    "I am a full-stack developer with a passion for building web and mobile applications. I have a strong background in front-end development and have worked with a wide range of technologies. I am currently working on a project that will allow me to create a mobile application that will allow users to create and share their own recipes.",
-  LearningStyles: [
-    {
-      title: "Bilateral or Homolateral personality",
-      desc: "I am a person who is very open to new ideas and challenges. I am very curious and enjoy learning new.",
-    },
-  ],
-  links: [
-    {
-      title: "Download CV",
-      link: "",
-    },
-    {
-      title: "Download Letter",
-      link: "",
-    },
-  ],
-};
-
-export const hobbiesPage = {
-  intro: "Most of us have free time, we just don’t spend it wisely.",
-  hobbies: [
-    {
-      title: "01. Key Findings",
-      desc: "To satisfy our goal of creating a brand that could adapt across many use cases, we took the base form of their new logo and used it as an artboard. As long as the structural outline of the mark stayed consistent their brand was allowed to flex and adapt to specific uses.",
-    },
-    {
-      title: "02. Goals",
-      desc: "o satisfy our goal of creating a brand that could adapt across many use cases, we took the base form of their new logo and used it as an artboard.",
-    },
-    {
-      title: "03. Problems",
-      desc: "To satisfy our goal of creating a brand that could adapt across many use cases, we took the base form of their new logo and used it as an artboard. As long as the structural outline of the mark stayed consistent their brand was allowed to flex and adapt to specific uses.",
-    },
-    {
-      title: "04. Solutions",
-      desc: "To satisfy our goal of creating a brand that could adapt across many use cases, we took the base form of their new logo and used it as an artboard. As long as the structural outline of the mark stayed consistent their brand was allowed to flex and adapt to specific uses.s",
-    },
-  ],
-  topImgs: [
-    {
-      url: "https://images.unsplash.com/photo-1533647046292-0187d1d2c978?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1567629733542-916a5af7e72a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-    },
-  ],
-  bottomImgs: [
-    {
-      url: "https://images.unsplash.com/photo-1619194999378-762462c38ab4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3027&q=80",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1600165642369-f609e6a2daa2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2992&q=80",
-    },
-  ],
-};
-
-export const projectsPage = {
-  intro: "Most of us have free time, we just don’t spend it wisely.",
-  projects: [
-    {
-      title: "A new project",
-      link: "",
-      module: "Appl com",
-    },
-  ],
-  highSchoolSubjects: [
-    "English Home Language",
-    "Afrikaans",
-    "Mathematics",
-    "Physical Science",
-    "Life Science",
-    "History",
-    "Life Orientation",
-  ],
-  modules: [
-    "Engineering Skills",
-    "Social Intelligence",
-    "Maths 1",
-    "Physics 1",
-    "Engineering Chemistry 1",
-    "ICT Skills",
-    "Applied Communication",
-  ],
 };
